e099382d4552248dcfb9ccb23853e8bee8cd97d7
elinakki <n1rael00@students.oamk.fi>
2022-09-19 13:15:50 +0300

    Luotu ostoslista useEffectillä

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [lista, setLista] = useState([])
+
+  useEffect(() => {
+    const ostoslista = Array()
+
+    ostoslista.push("Kahvi")
+    ostoslista.push("Maitoa")
+    ostoslista.push("Keksejä")
+
+    setLista(ostoslista)
+
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '30px'}}>
+    <h3>Ostoslista</h3>
+
+    
+
     </div>
   );
 }
